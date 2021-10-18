5f446ff5c6f005562b06e591a2d575e9f93f0d20
Laura GG <laura.garcia2.mt@correo.usa.edu.co>
2021-10-18 12:06:45 -0500

    1 version

---
+++ src/main/java/reto3/USA/reto3/USA/Application.java
@@ -0,0 +1,13 @@
+package reto3.USA.reto3.USA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/reto3/USA/reto3/USA/ClientInterface.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package reto3.USA.reto3.USA;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Laura
+ */
+public interface ClientInterface extends CrudRepository<Client, Integer> {
+    
+}
